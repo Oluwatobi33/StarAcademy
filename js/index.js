6d7279e513d18c2802cd1a74dd40e54ab8026028
Oluwatobi33 <bakareoluwatobi22@gmail.com>
2023-09-10 15:21:14 +0100

    I made changes to the landing page contact signup

--- js/index.js
+++ js/index.js
@@ -25,16 +25,63 @@ const Information = () => {
 var sidebarOpen = false;
 var sidebar = document.getElementById('sidebar');
 
-function openSidebar() {
-    if (!sidebarOpen) {
-        sidebar.classList.add('sidebar-responsive');
-        sidebarOpen = true;
+// function openSidebar() {
+//     if (!sidebarOpen) {
+//         sidebar.classList.add('sidebar-responsive');
+//         // sidebarOpen = true;
+//     } else if (!sidebarOpen) {
+//         sidebarOpen = `${style = "display:none"}`
+//     }
+// }
+
+// function closeSidebar() {
+//     if (sidebarOpen) {
+//         sidebar.classList.remove('sidebar-responsive');
+//         sidebarOpen = false;
+//     }
+// }
+
+
+function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    let sidebar = document.getElementById("main").style.marginLeft = "250px";
+    if (sidebar) {
+        document.getElementById("openbtn").style.display = "none";
     }
 }
 
-function closeSidebar() {
-    if (sidebarOpen) {
-        sidebar.classList.remove('sidebar-responsive');
-        sidebarOpen = false;
+function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+    if (sidebar) {
+        document.getElementById("openbtn").style.display = "visbile";
     }
 }
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+const scrollUp = () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    })
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function (event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
+
+$('body').toggleClass('loaded');
+
